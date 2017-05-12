f94f2f62e1811e320f0825965388db9d1c79fdb9
Alexiscbn <alexiscablan@yahoo.fr>
2017-05-12 11:10:38 +0200

    Projet

--- Site.php
+++ Site.php
@@ -8,7 +8,7 @@
 
 <body>
 <img src="ConnectUs.png" alt="Logo" />
-<h2>Connectez-vous à votre interface <strong> ConnectUs </strong> pour accéder à vos données personnelles. </h2>
+
 
 <form method="post" action="traitement.php">
 
@@ -18,19 +18,20 @@
     $heure = date("H:i");
     echo "Nous sommes le $date et il est $heure";
     ?>
-    <p>
+
     <div class="test">
+        <h2>Connectez-vous à votre interface <strong> ConnectUs </strong> pour accéder à vos données personnelles. </h2>
         <label>Nom d'utilisateur</label> : <input type="text" name="Identifiant" />
         </br><label for="pass"> Mot de passe :</label>
         <input type="password" name="Mot de passe" />
 
     </br>
     <input type="button" value="Connexion">
-    <a href="Inscription.html">
-        <input type="button" value="S'inscrire"></a>
+        <a href="Inscription.php"><input type="button" value="S'inscrire"></a>
+
     </br><input type="button" value="Aide">
     </br><input type="button" value="Contactez-nous">
-    </p>
+
 </div>
 </form>
 
