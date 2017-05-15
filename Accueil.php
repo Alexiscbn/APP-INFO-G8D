46bcff5b22a904f2cdd05f5cbef773b41967b6ff
Alexiscbn <alexiscablan@yahoo.fr>
2017-05-15 10:06:59 +0200

    Projet

--- Accueil.php
+++ Accueil.php
@@ -1,12 +1,25 @@
 <!DOCTYPE HTML>
 <head>
     <meta charset = "utf-8" />
-    <link rel = "stylesheet" href="style.css"/>
+    <link rel = "stylesheet" href="Style.css"/>
     <title>Plateforme utilisateur ConnectUs</title>
 </head>
 
 <body>
-<h1>
-    Parametrez votre habitation
-</h1>
+<h1><u>Parametrez votre habitation</u></h1>
+
+</p><div class="Paramètres">
+    Afin de pouvoir accès à toutes les fonctionnatlités de ConnectUs met à votre disposition, nous avons besoin que vous définissiez clairement l'organisation de votre habitation :
+<form action="traitementappart.php" method="post">
+    <p>
+        <br><label>La superficie de votre appartement est de </label> : <input type="text" name="metre" id="metre" value=""   /> mètres carrés<br/>
+        <br><label>Votre appartement est contitué de </label> : <input type="text" name="pieces" id="pieces" value="" /> pièces <br />
+        <br> <input type="submit" value="Valider" />
+    </p>
+</form>
+</div>
+<div class="logo">
+    <img src="ConnectUs.png" width="200" alt="Logo" />
+</div>
 </body>
+
