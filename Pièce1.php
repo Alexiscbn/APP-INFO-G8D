9806476b3236d0d33f8d095d7d4ec56c70e007d7
Alexiscbn <alexiscablan@yahoo.fr>
2017-05-18 17:31:53 +0200

    Projet

--- Pièce1.php
+++ Pièce1.php
@@ -8,8 +8,11 @@
 <body>
     <h3>Voici les informations reçues par les capteurs situés dans la chambre</h3>
 
-<p>Tous les capteurs sont en marche et fonctionnels</p>
+<p>Tous les capteurs sont en marche et fonctionnels
 
 Voici les informations renvoyées par tous les capteurs situés dans la chambre :
 
+    Il fait actuellement 18 degrés, la fenêtre est vérrouillée. Les volets sont ouverts et les valeurs reçues pas les detecteurs
+    de fumée et de CO2 sont parfaitement normales.
+</p>
 </body>
