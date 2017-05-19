4c11a4de4cebeb829c510c9bb19ec7861b3651ee
Nourcode <noureddine.wow@laposte.net>
2017-05-19 09:39:18 +0200

    "page principal"

---
+++ Main.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" </meta>
+    <title> Connect Us Accueil</title>
+    <link rel="stylesheet" type="text/css" href="accueil.css">
+</head>
+<body>
+<div class="cadre">
+    <div class="haut"> <p class="affichage">Détecteur</p></div>
+    <div class="centre">
+        <p class="imagecentre">
+            <img src="detecteur.jpg" alt="detecteur"/>
+        </p>
+        <p class="etat"> 25% </p>
+    </div>
+    <div class="bas">
+        <p class="affichage">On</p>
+    </div>
+</div>
+</body>
+</html>
