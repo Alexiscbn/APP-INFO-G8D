85d3636baa11162e5eb2ab8ef133dbc18529c7e5
Antoine PELISSIER <antoinepelissier78@orange.fr>
2017-05-19 18:31:39 +0200

    "capteur de fenetre"

---
+++ volet.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset = "utf-8" />
+    <link rel = "stylesheet" href="style2.css"/>
+    <title>volet</title>
+</head>
+<div class="devices">
+    <label>Devices</label> </div>
+<div class="stat">
+    <label>Statistiques</label>
+</div>
+<div class="dt">
+    <label>Donnee en temps reel</label>
+</div>
+<div class="donnee_temps_reel">
+    </br></br>
+    <p><label>Donnee en temps reel</label> </p>
+    </br></br></br></br></br>
+</div>
+<div class="voletss">
+
+    <label>volets</label></div>
+</br></br></br>
+<p>
+<div class="volet">
+    <label>volet electrique1</label>:<button class="onoff" onclick="onoff(this)"><div>off</div></button>
+    </br></br></br></br></br></br></br></br></br></br>
+    </br><label>volet electrique2</label>:<button class="onoff" onclick="onoff(this)"><div>off</div></button>
+</div>
+</p>
+<form>
+    </br></br></br></br>
+    <input type="button" value="Retour" onclick="history.go(-1)">
+</form>
+<div class="logo">
+    <img src="ConnectUs.png" width="200" alt="Logo" />
+</div>
+</form>
+
+<body>
+
+</body>
+</html>
