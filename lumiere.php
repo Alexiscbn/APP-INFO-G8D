95c06159552ed0b917148befe2dd4f4fd0e299ae
Antoine PELISSIER <antoinepelissier78@orange.fr>
2017-05-19 18:27:40 +0200

    "capteur de luminosité"

---
+++ lumiere.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset = "utf-8" />
+    <link rel = "stylesheet" href="style2.css"/>
+    <title>Capteur de luminosite</title>
+</head>
+
+<body>
+<div class="devices">
+    <label>Devices</label> </div>
+<div class="stat">
+    <label>Statistiques</label>
+</div>
+<div class="dt">
+    <label>Donnee en temps reel</label>
+</div>
+<div class="donnee_temps_reel">
+    </br></br>
+    <p><label>Donnee en temps reel</label> </p>
+</div>
+
+
+<div method="post" action="traitement.php">
+    </br></br></br></br></br></br></br></br></br></br></br></br></br>
+
+    <p>
+    <div class="capteur_luminosite">
+
+        <label>luminosite:</label>:<input type="number" name="valeur"/>
+
+    </div>
+</div>
+<form>
+    </br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br>
+    <input type="button" value="Retour" onclick="history.go(-1)">
+</form>
+<div class="logo">
+    <img src="ConnectUs.png" width="200" alt="Logo" />
+</div>
+</form>
+
+
+
+
+
+</form>
+</body>
