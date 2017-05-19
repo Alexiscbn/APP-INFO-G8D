cc128e407f74200cb978cd8ab565e1b9c9838621
Antoine PELISSIER <antoinepelissier78@orange.fr>
2017-05-19 18:29:27 +0200

    "capteur de temperature"

---
+++ temperature.php
@@ -0,0 +1,58 @@
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset = "utf-8" />
+    <link rel = "stylesheet" href="style2.css"/>
+    <title>Devices</title>
+</head>
+
+<body>
+<div class="devices">
+    <label>Devices</label> </div>
+<div class="stat">
+    <label>Statistiques</label>
+</div>
+<div class="dt">
+    <label>Donnee en temps reel</label>
+</div>
+<div class="donnee_temps_reel">
+    </br></br>
+    <p><label>Donnee en temps reel</label> </p>
+</div>
+</br></br></br></br>
+<div class="temp_int">
+    <label>temperature interieur</label> : <input type="number" name="valeur" /></div>
+
+
+
+<div method="post" action="traitement.php">
+    </br></br></br></br></br></br>
+
+    <p>
+    <div class="temperature">
+
+        <form action="site" method="post">
+
+        </form><label>radiateur 1</label>:<input type="number" name="valeur1"/><INPUT TYPE="submit"name="valeur1" VALUE="valider">
+        </br></br></br></br></br></br>
+        </br><label>radiateur 2</label>:<input type="number" name="valeur2"/>
+        </br></br></br></br></br></br>
+        </br><label>radiateur 3</label>:<input type="number" name="valeur3"/>
+    </div>
+</div>
+</form>
+<form>
+    </br></br></br></br>
+    <input type="button" value="Retour" onclick="history.go(-1)">
+</form>
+<div class="logo">
+    <img src="ConnectUs.png" width="200" alt="Logo" />
+</div>
+</form>
+
+
+
+
+
+</form>
+</body>
