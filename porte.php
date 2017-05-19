3b76bf622756c3d03db897f459905a727df7bae1
Antoine PELISSIER <antoinepelissier78@orange.fr>
2017-05-19 18:25:00 +0200

    "capteur de porte"

---
+++ porte.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset = "utf-8" />
+    <link rel = "stylesheet" href="style2.css"/>
+    <title>portes</title>
+</head>
+<div class="devices">
+    <label>Devices</label> </div>
+<div class="stat">
+    <label>Statistiques</label>
+</div>
+<div class="dt">
+    <label>Donnee en temps reel</label>
+</div>
+<div class="donnee_temps_reel">
+    </br></br>
+    <p><label>Donnee en temps reel</label> </p>
+</div>
+</br></br></br></br>
+<div class="portes"><label>portes</label></div>
+</br></br></br></br></br></br></br></br></br></br></br></br>
+
+<div class="porte">
+
+    <label>porte1</label>:<button class="onoff" onclick="onoff(this)"><div>off</div></button>
+    </br></br></br></br></br></br></br></br></br></br></br></br>
+    </br><label>porte2</label>:<button class="onoff" onclick="onoff(this)"><div>off</div></button>
+</div>
+<form>
+    </br>
+    <input type="button" value="Retour" onclick="history.go(-1)">
+</form>
+<div class="logo">
+    <img src="ConnectUs.png" width="200" alt="Logo" />
+</div>
+</form>
+
+<body>
+
+</body>
+</html>
