848d723b99555f21c7fcc0c03944f1a46669fb49
Antoine PELISSIER <antoinepelissier78@orange.fr>
2017-05-19 18:21:31 +0200

    "capteur de présence"

---
+++ presence.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset = "utf-8" />
+    <link rel = "stylesheet" href="style2.css"/>
+    <title>capteurs de presences</title>
+</head>
+<div class="devices">
+    <label>Devices</label> </div>
+<div class="stat">
+    <label>Statistiques</label>
+</div>
+<div class="dt">
+    <label>Donnee en temps reel</label>
+</div>
+<div class="donnee_temps_reel">
+    </br></br>
+    <p><label>Donnee en temps reel</label> </p>
+</div>
+</br></br>
+<div class="presence">
+    <label>capteurs de presences</label></div>
+<p>
+<div class="capteur_de_presences">
+    <label>Detection d un individu: camera chambre </label>:<button class="onoff" onclick="onoff(this)"><div>off</div></button>
+    </br></br></br></br></br></br>
+    </br><label>Detection d un individu: camera cuisine</label>:<button class="onoff" onclick="onoff(this)"><div>off</div></button>
+    </br></br></br></br></br></br>
+    </br><label>Detection d un individu: camera piece principale</label>:<button class="onoff" onclick="onoff(this)"><div>off</div></button>
+    </br></br></br></br></br></br>
+    </br><label>Detection d un individu: camera entree</label>:<button class="onoff" onclick="onoff(this)"><div>off</div></button>
+</div>
+</p>
+<form>
+    </br></br></br></br>
+    <input type="button" value="Retour" onclick="history.go(-1)">
+</form>
+<div class="logo">
+    <img src="ConnectUs.png" width="200" alt="Logo" />
+</div>
+</form>
+
+<body>
+
+</body>
+</html>
