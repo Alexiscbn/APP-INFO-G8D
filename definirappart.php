719ed57ac69db3dff056eba9427c9b289fa3850b
Nourcode <noureddine.wow@laposte.net>
2017-05-21 20:18:19 +0200

    définir un appartement

---
+++ definirappart.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8"/>
+<p>
+    Définissez votre appartement :
+
+</p>
+<form action="traitementappart.php" method="post">
+    <p>
+        <label>La superficie</label> : <input type="text" name="metre" id="metre" value=""   /> <br />
+        <label>Le nombre de pièces</label> : <input type="text" name="pieces" id="pieces" value="" /> <br />
+        <input type="submit" value="Valider" />
+    </p>
+</form>
+</html>
